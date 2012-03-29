9ee9bd295d82419793a94958285640d2acb52265
pepper pot <trilobyte@aktivix.org>
2012-03-29 14:38:30 +0100

    commit

--- incident/views.py
+++ incident/views.py
@@ -2,7 +2,7 @@ from django.core.paginator import Paginator, InvalidPage, EmptyPage
 from django.core.context_processors import csrf
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
-from models import Incident, Cop, Force
+from models import Incident, Cop, Force, Images
 from forms import IncidentForm, CopForm, ImagesForm
 from django.template import RequestContext, Context, loader
 from django.core.exceptions import ObjectDoesNotExist
@@ -94,32 +94,55 @@ def force_list(request, badge_code):
 							 
 #publish forms
 
+
 def new(request):
+	cop_form_set = modelformset_factory(Cop, extra=2,)
+	image_form_set = modelformset_factory(Images, extra=4)
 	c = {}
 	c.update(csrf(request))
-	c.update({'imagesform': ImagesForm().as_p()})
 	if request.method == 'POST':#if form has been submitted
-		form = IncidentForm(request.POST, request.FILES) # a form bound to the POST data
-		c.update({'form' : form})
-		if form.is_valid(): #All validation rules pass
-			i = form.save()
-			if request.FILES:
-				i.image = request.FILES['image']
-				i.save()
-#			return HttpResponseRedirect('/incident/editcop_%s'% i.cop.all()[0]) # redirect after POST
-			return HttpResponseRedirect('/incident/%s'%i.id)	
+		incidentForm = IncidentForm(request.POST, prefix='incident') # a form bound to the POST data
+		copFormSet = cop_form_set(request.POST, prefix='cop')
+		imageFormSet = image_form_set(request.POST, request.FILES, prefix='images')
+		if incidentForm.is_valid() and copFormSet.is_valid() and imageFormSet.is_valid():
+			cops = copFormSet.save()
+			images = imageFormSet.save()
+			incident = incidentForm.save()
+			for cop in request.POST['incident-cop_string'].split(','):
+				incident.cop.add(Cop.objects.get(badge=cop.strip()))
+			for image in images:
+				incident.image.add(image)
+				image.date = incident.date
+				image.save()
+			incident.save()	
+			return HttpResponseRedirect('/incident/%s'%incident.id)	
 	else:
-		form = IncidentForm() # an unbound form
-		c.update({'form': form, 'ImagesForm': ImagesForm()})
-	return render_to_response('new.html', c,
+		incidentForm = IncidentForm(prefix='incident') # an unbound form
+		copFormSet = cop_form_set(queryset = Cop.objects.none(),prefix='cop')
+		imageFormSet = image_form_set(queryset = Images.objects.none(), prefix='images')
+	c.update({'incident_form': incidentForm, 'cop_form': copFormSet, 'image_form': imageFormSet})
+	return render_to_response('test/new.html', c,
 		 context_instance=RequestContext(request))
-
-def cop_formset(request, coplist):
-	coplist = [cop.strip() for cop in coplist.split(',')]
-	cop_number = len(coplist)
-	form = modelformset_factory(CopForm, extra=cop_number)
-	form = form(initial = [{'badge': badge} for badge in coplist])
-	return HttpResponse(form.as_table())
+		 
+def copform(request,cop_string):
+	cop_list = [cop.strip().upper() for cop in cop_string.encode().split(',')]
+	try:
+		cop_exists_list = Cop.objects.filter(badge__in=cop_list)
+	except Cop.DoesNotExist:
+		cop_exists_list = []
+	cop_new_list = [cop for cop in cop_list if not cop in [c.badge for c in cop_exists_list]]
+	cop_new_tuples = []
+	for cop in cop_new_list:
+		try:
+			force = Force.objects.get(badge = re.search(r'^([\D]{1,3})', cop).groups()[0].upper())
+		except Force.DoesNotExist:
+			force = Force.objects.all()[0]
+		cop_new_tuples.append((cop, force))
+		
+	cop_form_set = modelformset_factory(Cop, extra=len(cop_new_list))
+	copFormSet = cop_form_set(queryset=Cop.objects.none(), prefix='cop', initial=[{'badge':badge, 'force':force}  for badge, force in cop_new_tuples])
+	return render_to_response('copformset.html', {'copformset': copFormSet, 'cop_exists': cop_exists_list})
+	
 
 def update_cop(request, badge):
   c = {}
@@ -141,51 +164,7 @@ def update_cop(request, badge):
   c = Context(c)
   return t.render(c)
   
-def copform(request,cop_string):
-	cop_list = [cop.strip() for cop in cop_string.encode().split(',')]
-	response = '' 
-	for cop in cop_list:
-		response += update_cop(request, cop)
-	return HttpResponse(response)
 
-def copsave(request, copstring):
-	badge, force, name = copstring.split(';')
-	force = Force.objects.get(id=force)
-	badge = badge.upper()
-	cop, new = Cop.objects.get_or_create(badge=badge)
-	cop.name = name
-	cop.force = force
-	cop.save()
-	return HttpResponse('Name: %s, Badge: %s, Force: %s CREATED'%(cop.name, cop.badge, cop.force))
-
-def imagesform_ajax(request):
-#	c = {}
-#	c.update(csrf(request))
-#	if request.method == 'POST':
-	form = ImagesForm().as_p()
-	return HttpResponse(form)
-
-def imageform(request):
-	c = {}
-	c.update(csrf(request))
-	c.update({'imageform': ImagesForm().as_p()})
-	if request.method == 'POST':#if form has been submitted
-		form = ImagesForm(request.POST, request.FILES) # a form bound to the POST data
-		c.update({'imageform' : form})
-		if form.is_valid(): #All validation rules pass
-			i = form.save()
-			if request.FILES:
-				i.image = request.FILES['image']
-				i.save()
-#			return HttpResponseRedirect('/incident/editcop_%s'% i.cop.all()[0]) # redirect after POST
-			return HttpResponseRedirect('/incident/recent')	
-	else:
-		form = ImagesForm() # an unbound form
-		c.update({'imageform': form,})
-	return render_to_response('partials/imageform.html', c,
-		 context_instance=RequestContext(request))
-def search(request):
-	return HttpResponse('search')
 	
 def cop_search(request):
 	return search(request, Cop)	
